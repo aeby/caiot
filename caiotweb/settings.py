0c50ef7e8757e79cbd062e954657eec2e8e62911
Reto Aebersold <aeby@substyle.ch>
2017-11-28 23:23:18 -0600

    fix hosts

--- caiotweb/settings.py
+++ caiotweb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.1.17']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
